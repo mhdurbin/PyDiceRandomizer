dc74bdd99a6bd02078576d192e353539bdee7271
mhdurbin <mhdurbin@users.noreply.github.com>
2020-05-05 21:50:55 -0600

    Updated and added to GitHub

---
+++ Test.py
@@ -0,0 +1,35 @@
+import random
+
+class Dice_Randomizer:
+
+    type_prompt = input ("Enter the dice roll(s) in following format (#d# # of times): ")
+
+    dicenum_num = int(type_prompt[0])
+    type_num = int(type_prompt[2])
+    iter_num = int(type_prompt[4])
+
+    rolls = []
+
+    print ("Below are the results for rolling", dicenum_num, "d", type_num, "dice", iter_num, "times.")
+
+    for i in range(iter_num):
+        rolls.append([])
+        for j in range(dicenum_num):
+            rolls[i].append(random.randint(1, type_num))
+    
+    print (rolls)
+
+    sum_prompt = input ("Would you like to sum the results? (Yes or No) " )
+
+    temp = 0
+    sum = []
+    if sum_prompt == "Yes":
+        for i in range(iter_num):
+            for j in range(dicenum_num):
+                temp += rolls[i][j]
+            sum.append(temp)
+            print (temp)
+            temp = 0
+        print (sum)
+    else:
+        print (rolls)
